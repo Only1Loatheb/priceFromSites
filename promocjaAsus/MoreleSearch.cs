db3eb10717f422a3ed21601999bbbf30db3d591d
Tomek <zielinski.x360@gmail.com>
2017-06-08 14:24:14 +0200

    optimization

--- promocjaAsus/MoreleSearch.cs
+++ promocjaAsus/MoreleSearch.cs
@@ -15,10 +15,33 @@ namespace promocjaAsus
 
         public string getUrlToFirstFromSearchUrl(string search) {
             HtmlDocument doc = web.Load(search);
-            String cheapestProduct = domain + doc.DocumentNode.SelectSingleNode(first).GetAttributeValue("href", "https://www.morele.net/procesor-intel-core-i7-6950x-3ghz-25-mb-box-bx80671i76950x-774250/"); //DocumentNode.SelectSingleNode("//div[@id='comic']//img");
+            String cheapestProduct = domain + doc.DocumentNode.SelectSingleNode(first).GetAttributeValue("href", "https://www.morele.net/procesor-intel-core-i7-6950x-3ghz-25-mb-box-bx80671i76950x-774250/");
             return cheapestProduct;
         }
-        public string getUrlToNFromSearchUrl(string search,int n)
+
+        public List<string> getUrlsToNFromSearchUrl(string search, int n)
+        {
+            HtmlDocument doc = web.Load(search);
+            var links = new  List<string>();
+            for (int i = 1; i <= n; ++i)
+            {
+                int tableNo = (int)Math.Floor((double)i / 3.0) + 1;
+                int rowNo = i % 3 + 1;
+                string selector = String.Format("//*[@id=\"content\"]/div/div[2]/div[2]/div[2]/div[7]/div[{0}]/div/div[{1}]/div/div[2]/div[1]/div[1]/div[2]/a",
+                    tableNo, rowNo);
+                string href = doc.DocumentNode.SelectSingleNode(selector).GetAttributeValue("href", "");
+                if (href == "")
+                {
+                    return links;
+                }
+                StringBuilder sb = new StringBuilder(domain);
+                sb.Append(href);
+                links.Add(sb.ToString());
+            }
+            return links;
+        }
+
+        public string getUrlToIFromSearchUrl(string search,int n)
         {
             int tableNo = (int) Math.Floor((double)n / 3.0) + 1;
             int rowNo = n % 3 + 1;
--- promocjaAsus/Program.cs
+++ promocjaAsus/Program.cs
@@ -18,12 +18,20 @@ namespace promocjaAsus
              MoreleSearch search = new MoreleSearch();
             const string priceAscRam3000mhz8gbDDR4 = "https://www.morele.net/komputery/podzespoly-komputerowe/pamieci-ram-38/,,,,,,,p,0,,7965O980523,7971O459459,7973O811795/1/";
             var ram = new List<Tuple<decimal, string>>();
+            var links = search.getUrlsToNFromSearchUrl(priceAscRam3000mhz8gbDDR4, 6);
+            Parallel.ForEach(links, link => {
+                ram.Add(new Tuple<decimal, string>(morele.getPriceFromUrl(link), link));
+            });
+            ram.Sort((r1, r2) => r1.Item1.CompareTo(r2.Item1));
+            ram.ForEach(r => Console.WriteLine("Cena: {0} Link: {1}", r.Item1, r.Item2));
+            /*
             Parallel.For(1, 6, i => {
                 string link = search.getUrlToNFromSearchUrl(priceAscRam3000mhz8gbDDR4, i);
                 ram.Add(new Tuple<decimal, string>(morele.getPriceFromUrl(link), link));
             });
-            ram.Sort((r1,r2)=>r1.Item1.CompareTo(r2.Item1));
-            ram.ForEach(r => Console.WriteLine("Cena: {0} Link: {1}",r.Item1,r.Item2));
+            */
+            //ram.Sort((r1,r2)=>r1.Item1.CompareTo(r2.Item1));
+            //ram.ForEach(r => Console.WriteLine("Cena: {0} Link: {1}",r.Item1,r.Item2));
             //string link = search.getUrlToFirstFromSearchUrl(priceAscRam3000mhz8gbDDR4);
             //Console.WriteLine("Najtańszy RAM: {0} link: {1}", morele.getPriceFromUrl(link), link);
 
